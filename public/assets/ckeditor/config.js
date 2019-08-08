06f92075dbc9cf687b4a978abc6bcb49528a386f
Edo Lorenza <edolorenza@Edos-MacBook.local>
2019-08-08 11:23:20 +0700

    add plugin ck editor

---
+++ public/assets/ckeditor/config.js
@@ -0,0 +1,20 @@
+/**
+ * @license Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+CKEDITOR.editorConfig = function( config ) {
+	// Define changes to default configuration here. For example:
+	// config.language = 'fr';
+	// config.uiColor = '#AADC6E';
+
+};
+
+
+CKEDITOR.editorConfig = function( config ) {
+    // Define changes to default configuration here. For example:
+    // config.language = 'fr';
+    // config.uiColor = '#AADC6E';
+    config.extraPlugins = "lineutils,widget,codesnippet";
+    config.codeSnippet_theme = 'monokai_sublime';
+};
